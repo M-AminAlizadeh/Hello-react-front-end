d580583f5fd4636b7c33c9669abedd318c4eacaa
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-23 16:24:31 +0330

    Add redux to the app

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,21 +1,24 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setGreeting } from '../redux/actions';
 
 const Greetings = () => {
-  const [greeting, setGreeting] = useState('');
+  const greeting = useSelector((state) => state.greeting);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchGreetings = async () => {
       const response = await fetch('http://127.0.0.1:3000/greetings/random');
       if (response.ok) {
         const data = await response.json();
-        setGreeting(data.greeting);
+        dispatch(setGreeting(data.greeting));
       } else {
         console.log(response);
       }
     };
 
     fetchGreetings();
-  }, []);
+  }, [dispatch]);
 
   return (<h1>{greeting}</h1>);
 };
